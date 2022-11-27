72fa5179628dbddfeef8adb4f68ea3d4fffbd2df
Gzm <979218087@qq.com>
2022-11-27 21:41:53 +0800

    轮播图 for 优化

--- Musicapp/entry/src/main/js/MainAbility/pages/Mainpage/Mainpage.js
+++ Musicapp/entry/src/main/js/MainAbility/pages/Mainpage/Mainpage.js
@@ -1,8 +1,10 @@
 export default {
     data: {
-        title: ""
+        title: "",
+        swiperImage: []
     },
     onInit() {
         this.title = "Hello World";
+        this.swiperImage = ["https://s2.best-wallpaper.net/wallpaper/1920x1080/1712/Furry-dog-front-view-eyes_1920x1080.jpg"];
     }
 }
