5d4a4af293871abd0dbb67010a9f673d9a7d077d
Gzm <979218087@qq.com>
2022-12-14 23:21:06 +0800

    消息搜索

--- Musicapp/entry/src/main/js/MainAbility/pages/Message/Message.js
+++ Musicapp/entry/src/main/js/MainAbility/pages/Message/Message.js
@@ -1,7 +1,9 @@
+
 export default {
     data: {
         title: "消息",
         message_shop: [],
+        value: "",
     },
     onInit() {
         this.title = "消息";
@@ -11,17 +13,10 @@ export default {
             {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
             {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
             {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
-            {url: "/Img/Leaftree.png" , Title : "Leaftree旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
+            {url: "/Img/油画.jpg" , Title : "油画旗舰店" , date : "星期一" , message : "如果消息较多打扰到您。可以点此拒绝消息"},
         ]
+    },
+    change(e){
+        this.value =e.value;
     }
 }
